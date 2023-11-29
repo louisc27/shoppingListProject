412e8143d02dbdfa179677c6f008b35365a39a4a
LouisCunningham <cunningtechgroup@outlook.com>
2023-11-29 16:05:25 -0500

    add/remove item complete

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter')
+
 
 
 // Function to add items to list
@@ -24,8 +27,10 @@ function addItem(e) {
  
  const button = createButton('remove-item btn-link text-red');
  li.appendChild(button);
-
+// Add li to the DOM
  itemList.appendChild(li);
+
+ checkUI();
 
  itemInput.value = '';
 }
@@ -47,5 +52,41 @@ function createIcon(classes){
   return icon;
 }
 
+function removeItem(e){
+  //if the targeted events parent element contains the class of remove-item
+  if (e.target.parentElement.classList.contains('remove-item')){
+    //within the event parameter target the parent element (button) then the parent element of that (the list item) then remove it
+    if(confirm('Are you sure?')){
+    e.target.parentElement.parentElement.remove();
+
+    checkUI();
+    }
+  }
+}
+
+function clearItem(){
+  while(itemList.firstChild){
+    itemList.removeChild(itemList.firstChild);
+  }
+  checkUI();
+}
+
+function checkUI(){
+  const items = itemList.querySelectorAll('li');
+  if(items.length === 0){
+    clearBtn.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else {
+    clearBtn.style.display = 'block';
+    itemFilter.style.display = 'block';
+  }
+}
+
+
+
 //Event Listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItem);
+
+checkUI();
